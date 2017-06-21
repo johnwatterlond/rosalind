69890031edae093ef8a1e9ffa9b9810a81314f69
john watterlond <john.watterlond@gmail.com>
2017-06-20 22:49:51 -0700

    Script now automatically grabs input file from datasets directory.

--- scripts/rna.py
+++ scripts/rna.py
@@ -3,22 +3,25 @@ Program to solve the Rosalind problem at rosalind.info/problems/rna/
 
 """
 
-def dna_str_to_rna_str(s):
-    """Convert DNA string s to RNA string."""
-    return s.replace('T', 'U')
+def dna_str_to_rna_str(dna_string):
+    """Convert DNA string dna_string to RNA string."""
+    return dna_string.replace('T', 'U')
 
 
 def solve_problem(s):
-    """Create file in cwd 'rna_answer.txt' containing solution."""
-    fout = open('rna_answer.txt', 'w')
-    fout.write(dna_str_to_rna_str(s))
+    """Create file '../output/rna_answer.txt' containing solution."""
+    fout = open('../output/rna_answer.txt', 'w')
+    rna_string = dna_str_to_rna_str(s)
+    fout.write(rna_string)
     fout.close()
 
 
 def main():
-    f = input('Location of rosalind_rna.txt?\n> ')
-    s = open(f).read().strip()
-    solve_problem(s)
+    location = '../datasets/rosalind_rna.txt'
+    fin = open(location)
+    dna_string = fin.readline().strip()
+    solve_problem(dna_string)
+    fin.close()
 
 
 if __name__ == '__main__':
