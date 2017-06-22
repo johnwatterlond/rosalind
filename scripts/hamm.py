703af19e616f010efb25cd54e45585af888e7f70
john watterlond <john.watterlond@gmail.com>
2017-06-21 22:20:01 -0700

    Script now automatically grabs input file from datasets directory.

--- scripts/hamm.py
+++ scripts/hamm.py
@@ -4,20 +4,25 @@ Program to solve the Rosalind problem at rosalind.info/problems/hamm/
 """
 
 def hamm_dist(s1, s2):
-    """Return the Hamming distance between the strings s1 and s2."""
+    """Return the Hamming distance between the DNA strings s1 and s2."""
     return len([l1 for (l1, l2) in zip(s1, s2) if l1 != l2])
 
 
 def solve_problem(s1, s2):
-    print(hamm_dist(s1, s2))
+    """Create file '../output/hamm_answer.txt' containing solution."""
+    fout = open('../output/hamm_answer.txt', 'w')
+    distance = hamm_dist(s1, s2)
+    fout.write(str(distance))
+    fout.close()
 
 
 def main():
-    location = input('Location of rosalind_hamm.txt?\n> ')
-    f = open(location)
-    s1 = f.readline()
-    s2 = f.readline()
+    location = '../datasets/rosalind_hamm.txt'
+    fin = open(location)
+    s1 = fin.readline().strip()
+    s2 = fin.readline().strip()
     solve_problem(s1, s2)
+    fin.close()
 
 
 if __name__ == '__main__':
