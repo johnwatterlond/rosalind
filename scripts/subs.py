83ca8632642ed123b805045e89fe6c27674b5a21
john watterlond <john.watterlond@gmail.com>
2017-06-25 19:43:18 -0700

    Script now automatically grabs input file from datasets directory. Cleaned up find_all_sub.

--- scripts/subs.py
+++ scripts/subs.py
@@ -3,22 +3,14 @@ Program to solve the Rosalind problem at rosalind.info/problems/subs/
 
 """
 
-
 def find_all_sub(s, sub):
     """
     Returns a list of indices (starting at 1) corresponding to locations
     of all occurences string sub in string s.
     """
-    indices = []
-    if s.find(sub) == 0:
-        indices.append(1)
-    i = 1
-    while i != 0:
-        x = s.find(sub, i)
-        if x != -1:
-            indices.append(x + 1)
-        i = x + 1
-    return indices
+    indices = {s.find(sub, i) for i in range(len(s) + 1) if s.find(sub, i) != -1}
+    indices_up_one = [index + 1 for index in indices]
+    return sorted(indices_up_one)
 
 
 def format_list_to_str(l):
@@ -27,19 +19,20 @@ def format_list_to_str(l):
 
 
 def solve_problem(s, sub):
-    """Create file in cwd 'subs_answer.txt' containing solution."""
-    fout = open('subs_answer.txt', 'w')
-    l = find_all_sub(s, sub)
-    fout.write(format_list_to_str(l))
+    """Create file '../output/subs_answer.txt' containing solution."""
+    fout = open('../output/subs_answer.txt', 'w')
+    sub_indices = find_all_sub(s, sub)
+    fout.write(format_list_to_str(sub_indices))
     fout.close()
 
 
 def main():
-    location = input('Location of rosalind_subs.txt?\n> ')
+    location = '../datasets/rosalind_gc.txt'
     fin = open(location)
     s = fin.readline().strip()
     sub = fin.readline().strip()
     solve_problem(s, sub)
+    fin.close()
 
 
 if __name__ == '__main__':
